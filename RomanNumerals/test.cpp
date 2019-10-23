591955c7b6d279a44ce43ea0ba10fc6628692bb1
mistermaxis <55718118+mistermaxis@users.noreply.github.com>
2019-10-23 14:37:18 -0300

    TEST 1R (Refactored test unit)

--- RomanNumerals/test.cpp
+++ RomanNumerals/test.cpp
@@ -1,5 +1,18 @@
 #include "pch.h"
 
+std::string arabic_to_roman(int);
+
+class ArabicRomanAssert {
+private:
+	const unsigned int arabicToConvert;
+public:
+	ArabicRomanAssert() = delete;
+	explicit ArabicRomanAssert(const unsigned int arabicNumber) : arabicToConvert(arabicNumber){}
+	void isConvertedToRoman(std::string romanNumber) const { ASSERT_EQ(romanNumber, arabic_to_roman(arabicToConvert)); }
+};
+
+ArabicRomanAssert assertThat(const unsigned int arabic_assert) { ArabicRomanAssert assert{ arabic_assert }; return assert; }
+
 int main(int argc, char** argv) {
 	testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
@@ -21,26 +34,6 @@ std::string arabic_to_roman(int arabic)
 	return roman;
 }
 
-TEST(RomanNumerals, 1_is_converted_to_I) {
-	ASSERT_EQ("I", arabic_to_roman(1));
-}
-
-TEST(RomanNumerals, 2_is_converted_to_II) {
-	ASSERT_EQ("II", arabic_to_roman(2));
-}
-
-TEST(RomanNumerals, 3_is_converted_to_III) {
-	ASSERT_EQ("III", arabic_to_roman(3));
-}
-
-TEST(RomanNumerals, 10_is_converted_to_X) {
-	ASSERT_EQ("X", arabic_to_roman(10));
-}
-
-TEST(RomanNumerals, 20_is_converted_to_XX) {
-	ASSERT_EQ("XX", arabic_to_roman(20));
-}
-
-TEST(RomanNumerals, 30_is_converted_to_XXX) {
-	ASSERT_EQ("XXX", arabic_to_roman(30));
+TEST(RomanNumerals, 33_is_converted_to_XXXIII) {
+	assertThat(33).isConvertedToRoman("XXXII");
 }
